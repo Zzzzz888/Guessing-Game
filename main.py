b80cc418cf75aeb9aa2f6ad9c858f4a0c52a0edb
Zzzzz888 <116314149+Zzzzz888@users.noreply.github.com>
2022-11-10 23:00:16 +0000

    yo

--- main.py
+++ main.py
@@ -30,33 +30,43 @@ while boolean:
 
 # 4. Generate a random number from the range 1 to the number the user entered, make sure it is outside your while loop - known as a global variable
 secret = random.randint (1, num)
-print(secret)
+
 # 5. Create two more variables - one for the guess and one for the count of guesses 
 guess = None
 count = 1
-
+gamecount = 1
 # 6a. Create a new while loop below these variables that runs while the guess vairable is not equal to the random number
-while guess != secret:
-  guess = input ("Type a number between 1 and " + str(num) + ": ")
-# 6b. Update the guess variable with input from the user, prompt them by saying "Type a number between 1 and" add the number they orginally entered
-  if guess.isdigit():
-    
-    guess = int(guess)
-   
+while gamecount < 4:
+  
+  while guess != secret and count<11:
+    guess = input ("Type a number between 1 and " + str(num) + ": ")
+  # 6b. Update the guess variable with input from the user, prompt them by saying "Type a number between 1 and" add the number they orginally entered
+    if guess.isdigit():
       
+      guess = int(guess)
+     
+        
+     
+  # 7. Verify the guess is a number (like before) and if it is, update the guess variable to be the value of itself as an int
+    
+    
    
-# 7. Verify the guess is a number (like before) and if it is, update the guess variable to be the value of itself as an int
-  
-  
- 
-# 8. Check it! Make an if else statement to see if guess equals the random number, if it does tell the user they won! If it does not, update the guess count number by 1
-  if guess == secret:
-    print("You Win!!!")
-
-  else:
-    print ("Try Again")
-# 9. Let the user know how many guesses it took.  Print a message using the guess count number - make sure it makes sense no matter what the number is
-  count+=1
-  print(count)
+  # 8. Check it! Make an if else statement to see if guess equals the random number, if it does tell the user they won! If it does not, update the guess count number by 1
+    if guess == secret:
+        gamecount +=1
+        print("You Win!!!")
+        if count==1:
+        
+          print("It took you",count,"try")
+        else:
+            print("It took you",count,"tries")
+    else:
+      print ("Try Again")
+  # 9. Let the user know how many guesses it took.  Print a message using the guess count number - make sure it makes sense no matter what the number is
+      count+=1
+      if guess > secret:
+         print("Lower")   
+      else:
+        print("Higher")
 # 10. Make the game playable more than once, add a while loop around all of your code
 
